1eb534c93ee7caaa66ddf2e419fdd9f37b7c6392
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2022-05-05 03:25:06 +0000

    RE546F: Update prebuilts to C.09

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ TARGET_OTA_ASSERT_DEVICE := RE546F
 
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
-#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 105934848 # This is the maximum known partition size, but it can be higher, so we just omit it
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 92299264 # This is the maximum known partition size, but it can be higher, so we just omit it
 BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
